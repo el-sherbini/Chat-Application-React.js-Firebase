91f9245d276fb5ea7024e56e651ae36536741d84
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-12 17:50:11 +0200

    prepare project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import { AuthProvider } from "./contexts/AuthContext";
+import Chats from "./components/Chats";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/chats" exact element={<Chats />} />
+            <Route path="/" exact element={<Login />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
