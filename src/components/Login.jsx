d0303075526ab53eee0843a955cca6f9bd33999e
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-13 16:20:52 +0200

    add login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+import { GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import { auth } from "../firebase";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login() {
+  return (
+    <div id="login-page">
+      <div id="login-card">
+        <h2>Welcome to Ichat!</h2>
+        <div
+          className="login-button google"
+          onClick={() => auth.signInWithRedirect(new GoogleAuthProvider())}
+        >
+          <GoogleOutlined /> Sign In with Google
+        </div>
+        <br /> <br />
+        <div
+          className="login-button facebook"
+          onClick={() => auth.signInWithRedirect(new FacebookAuthProvider())}
+        >
+          <FacebookOutlined /> Sign In with Facebook
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
