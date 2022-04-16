80a5a832935bc1049aa850d91740d28d9a75b031
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-16 16:12:37 +0200

    add chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
+import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  const navigate = useNavigate();
+  const { user } = useAuth();
+  const [Loading, setLoading] = useState(true);
+
+  console.log(user);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    navigate("/");
+  };
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+      return;
+    }
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": process.env.CHAT_ENGINE_PRODUCT_ID,
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formData = new FormData();
+        formData.append("email", user.email);
+        formData.append("username", user.email);
+        formData.append("secret", user.uid);
+        getFile(user.photoURL).then((avatar) => {
+          formData.append("avatar", avatar, avatar.name);
+        });
+        axios
+          .post("https://api.chatengine.io/users", formData, {
+            headers: { "private-key": process.env.CHAT_ENGINE_PRIVATE_KEY },
+          })
+          .then(() => setLoading(false))
+          .catch((error) => console.log(error));
+      });
+  }, [user, navigate]);
+
+  if (!user || Loading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Ichat</div>
+
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectID={process.env.CHAT_ENGINE_PRODUCT_ID}
+        userName={user.email}
+        userSecret={user.uid}
+      />
+    </div>
+  );
 };
 
 export default Chats;
